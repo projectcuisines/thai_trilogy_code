353549d45cb2dcbe022b0316a5417e71df7637ff
Thomas Fauchez <69323961+tfauchez@users.noreply.github.com>
2020-08-13 21:41:23 -0400

    Addition of rel and rei

--- model_rocke3d.py
+++ model_rocke3d.py
@@ -21,3 +21,95 @@ def adjust_rocke3d_grid(darr, lon_name="lon", lat_name="lat"):
             out.assign_coords(**{lon_name: out[lon_name] + 180}), lon_name=lon_name
         )
     return out
+def calc_rocke3d_rei(rocke3d_ds,
+	a0_agg_cold = 7.5094588E-04,
+	b0_agg_cold = 5.0830326E-07,
+	a0_agg_warm = 1.3505403E-04,
+	b0_agg_warm = 2.6517429E-05,
+	t_switch    = 216.208,
+	t0_agg      = 279.5,
+	s0_agg      = 0.05,
+):
+ 
+    """     Aggregate parametrization based on effective dimension.                                                                                                      
+      In the initial form, the same approach is used for stratiform                                                                                                
+      and convective cloud.                                                                                                                                        
+ 
+      The fit provided here is based on Stephan Havemann's fit of                                                                                                  
+      Dge with temperature, consistent with David Mitchell's treatment                                                                                             
+      of the variation of the size distribution with temperature. The                                                                                              
+      parametrization of the optical properties is based on De                                                                                                     
+      (=(3/2)volume/projected area), whereas Stephan's fit gives Dge                                                                                               
+      (=(2*SQRT(3)/3)*volume/projected area), which explains the                                                                                                   
+      conversion factor. The fit to Dge is in two sections, because                                                                                                
+      Mitchell's relationship predicts a cusp at 216.208 K. Limits                                                                                                 
+      of 8 and 124 microns are imposed on Dge: these are based on this                                                                                             
+      relationship and should be reviewed if it is changed. Note also                                                                                              
+      that the relationship given here is for polycrystals only. 
+      
+    Parameters
+    ----------
+    rocke3d_ds: ROCKE-3D data set
+    
+    These are the parameters used in the temperature dependent 
+	parameterizations for ice cloud particle sizes below.
+	Parameters for the aggregate parametrization
+	a0_agg_cold = 7.5094588E-04,
+	b0_agg_cold = 5.0830326E-07,
+	a0_agg_warm = 1.3505403E-04,
+	b0_agg_warm = 2.6517429E-05,
+	t_switch    = 216.208,
+	t0_agg      = 279.5,
+	s0_agg      = 0.05, 
+	
+	Returns
+    -------
+    rei: xarray.DataArray
+        Ice effective radius [um], dimension (time,lev,lat,lon).
+    """
+  	# Air temperature in ROCKE-3D
+    air_temp = rocke3d_ds.t 
+
+    # Calculate the R_eff
+    rei = xr.where(air_temp < t_switch,
+    
+    a0_agg_cold*da.exp(s0_agg *(air_temp - t0_agg)) + b0_agg_cold,
+     
+    a0_agg_warm*da.exp(s0_agg *(air_temp - t0_agg)) + b0_agg_warm)
+    
+    # Limit of the parameterization
+    rei = (3/2)*(3/(2 * da.sqrt(3))) * xr.ufuncs.minimum(1.24E-04, xr.ufuncs.maximum(8.0E-06,rei))
+
+#    # Convert to microns
+#    rei *= 1e-6
+    rei.rename("ice_cloud_condensate_effective_radius")
+    rei.attrs = {"long_name": "ice_cloud_condensate_effective_radius", "units": "micron"}
+    return rei
+    
+ def calc_rocke3d_rel(liq,
+	mw_dryair = 28.97*1e3,
+):   
+	 """     Calculation of the effective radius of liquid water
+	 following: 
+	 r_eff = ( 3 * rho_air * qcl / ( 4 * pi * rho_water * 0.8 * 1e8 ))**(1./3.)
+     corresponding to Eq 14 
+     in https://journals.ametsoc.org/jas/article/51/13/1823/23387/The-Measurement-and-Parameterization-of-Effective
+     Parameters
+    ----------
+    liq: xarray.DataArray
+    	mass mixing ratio of liquid [kg/kg].
+    mw_dryair : float, optional
+        Mean molecular weight of dry air [kg mol-1].
+    
+     Returns
+    -------
+    rei: xarray.DataArray
+        Ice effective radius [um], dimension (time,lev,lat,lon).
+    """  
+ 
+	rel = (3 * mw_dryair * liq / (4*np.pi*1000*0.8*1e8))**(1./3.)
+	
+	rel.rename("liquid_cloud_condensate_effective_radius")
+    rel.attrs = {"long_name": "liquid_cloud_condensate_effective_radius", "units": "micron"}
+    
+    return rel
