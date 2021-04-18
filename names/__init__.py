f103a034ea8f89fa1a0e8ac8abf9959639471226
Dennis Sergeev <dennis.sergeev@gmail.com>
2021-04-18 22:48:36 +0100

    add albedo and GHE to the table in THAI-CRE notebook

--- names/__init__.py
+++ names/__init__.py
@@ -1,4 +1,6 @@
 """Model-specific dictionaries of variable names and coordinates."""
+from dataclasses import dataclass
+
 from .exocam import exocam
 from .lmdg import lmdg
 from .rocke3d import rocke3d
@@ -6,8 +8,23 @@ from .um import um
 
 
 __all__ = (
+    "names",
     "exocam",
     "lmdg",
     "rocke3d",
     "um",
 )
+
+
+@dataclass
+class Names:
+    exocam: str = None
+    lmdg: str = None
+    rocke3d: str = None
+    um: str = None
+
+    def __getitem__(self, model_key):
+        return getattr(self, model_key.lower())
+
+
+names = Names(exocam=exocam, lmdg=lmdg, rocke3d=rocke3d, um=um)
